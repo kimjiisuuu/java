0a56377495d4622c64bfeb70d88bd4328b6405fd
kimjiisuuu <raina11170@gmail.com>
2024-10-14 00:12:31 +0900

    배열 연습

---
+++ day06/src/arrayTest/ArrayTest1.java
@@ -0,0 +1,21 @@
+package arrayTest;
+
+public class ArrayTest1 {
+
+	public static void main(String[] args) {
+		int[] ar = {1,2,3,4,5,};
+		System.out.println(ar[0]);
+		ar[0] =10;
+		System.out.println(ar[0]);
+		
+		int[] ar2 =new int[5];
+		System.out.println(ar2[3]);
+		System.out.println(ar2);
+		
+		int[] ar3 =null;
+//		String str2="hi";
+//		String str= new String("hi");
+		ar= new int[5];
+		System.out.println(ar3);
+	}
+}

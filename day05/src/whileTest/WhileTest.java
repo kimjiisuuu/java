3ee1b2b9291aba88eb79dc38badecf8eb8978a13
kimjiisuuu <raina11170@gmail.com>
2024-10-13 22:39:15 +0900

    while 연습

---
+++ day05/src/whileTest/WhileTest.java
@@ -0,0 +1,17 @@
+package whileTest;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+		
+//		for (int i = 0; i <   10; i++) {
+//			System.out.println("안녕");
+//		}
+//		
+		int cnt=0;
+		while(cnt<10) {
+			System.out.println("안녕");
+			cnt ++;
+		}
+	}
+}
---
+++ day05/src/whileTest/WhileTest2.java
@@ -0,0 +1,43 @@
+package whileTest;
+
+import java.util.Scanner;
+
+public class WhileTest2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String msg = "당신의 mbti는??\n" + "1.isfj\n" + "2. infj\n" + "3.esfp\n" + "4.entp\n"
+				+ "5. 종료" + "선택";
+		String isfj = "차분하고 인내심이 강하다", infj = "통찰력이 높고 사람들에게 영감을 준다", esfp = "그냥이상하다", entp = "최고의 유형 짱이다",
+				errMsg = "잘못입력했습니다";
+		String result = null;
+		int choice = 0;
+
+		while (true) {
+			System.out.println(msg);
+
+			choice = sc.nextInt();
+			
+			if(choice ==5) {break;}
+
+			switch (choice) {
+			case 1:
+				result = isfj;
+				break;
+			case 2:
+				result = infj;
+				break;
+			case 3:
+				result = esfp;
+				break;
+			case 4:
+				result = entp;
+				break;
+			default:
+				result = errMsg;
+
+			}
+			System.out.println(result);
+		}
+	}
+}
